a386be722628dbacb146f0b017c1d775cbf74979
Asher <mahdis.ghadamyari@gmail.com>
2024-06-15 01:48:00 +0330

    [fix]  superuser

--- university_panel/university_panel/settings.py
+++ university_panel/university_panel/settings.py
@@ -153,5 +153,4 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DJANGO_SUPERUSER_PASSWORD = 'root'
-DJANGO_SUPERUSER_USERNAME = 'root'
+
