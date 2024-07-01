9c63f935a61005f15961bb61a374ab2767495326
Asher <mahdis.ghadamyari@gmail.com>
2024-07-01 23:00:00 +0330

    cors allowed

--- university_panel/university_panel/settings.py
+++ university_panel/university_panel/settings.py
@@ -57,7 +57,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+]
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'access-control-allow-origin',
 ]
